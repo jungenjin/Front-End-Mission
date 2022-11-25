b0ef7faf23496bbbf8ed9a352dbbde24b2d84382
jej1217s1217 <jej1217s1217@gmail.com>
2022-11-25 18:19:40 +0900

    commit

--- javascript/script.js
+++ javascript/script.js
@@ -17,46 +17,6 @@ jQuery(document).ready(function () {
          $(this).find('.submenu').stop().slideUp(500);
       });
 
-   //    $(function () {
-   //       $('.bxslider').bxSlider({
-   //          slideWidth: 600,
-   //          //이미지 위에 텍스트 올리기
-   //          captions: true,
-   //          //슬라이드 전환 지속 시간 (다음 넘어가기 전까지)
-
-   //          speed: 1000,
-   //          //다음 전환까지 멈춰있는 시간 (보이는 시간)
-
-   //          pause: 3000,
-   //          //자동실행
-   //          auto: true,
-   //          //컨트롤버튼 (재생/정지) 표시
-   //          autoControls: true,
-   //          //클릭해서 자동실행 멈출 수 있도록 지정
-   //          stopAutoOnClick: true,
-   //       });
-   //    });
-   //    // .imgslide a:gt(0) : gt(index)은 index 값보다 더 큰 값(Greater)을
-   //    // 가진 요소를 선택함. 0번째부터 계수하여 index 값보다 큰 값을 가져옴.
-   //    // 여기서는 gt(0)이므로 1, 2, 3이 됨.
-   //    $('.imgslide a:gt(0)').hide();
-   //    // setInterval(function(){}), 3000 : 일정시간마다 반복적으로
-   //    // 동작을 실행하게 함. 3000은 3000ms(3초)는 3초마다 반복 실행함.
-   //    setInterval(function () {
-   //       // 가상클래스 선택자로 부모 요소가 가지고 있는 자식 요소 중 첫번째를
-   //       // 선택함. imgslide의 자식요소 <a>태그에서 첫번째 선택하여
-   //       // 페이드아웃을 실행함.
-   //       $('.imgslide a:first-child')
-   //          .fadeOut()
-   //          // 다음 태그를 선택하여 페이드인 실행함
-   //          .next('a')
-   //          .fadeIn()
-   //          // end() 메서드로 끝에다 appendTo() 함수 내용을 붙여줌
-   //          // 즉, 선택한 태그를 .imgslide 선택자 요소의 자식 요소로 추가해줌
-   //          .end()
-   //          .appendTo('.imgslide');
-   //    }, 3000);
-
    // (".notice li:first").click : .notice 요소의 후손 요소
    // 중에서 첫 번째 요소를 클릭하면
    $('.notice li:first').click(function () {
@@ -70,6 +30,7 @@ jQuery(document).ready(function () {
       $('#modal').removeClass('active');
    });
 });
+//cookie
 $(function () {
    //쿠키("popup")의 값이 'none'이면 id 값이 인 요소를 숨김니다.
    if ($.cookie('popup') == 'none') {
